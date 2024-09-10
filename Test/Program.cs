7bfa316e73065f4ebe628236f4170fe91706377e
sjsong <gkwlak645@gmail.com>
2024-09-10 18:21:28 +0900

    first

---
+++ Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK;
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+using System.Runtime.InteropServices;
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK.Types;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
